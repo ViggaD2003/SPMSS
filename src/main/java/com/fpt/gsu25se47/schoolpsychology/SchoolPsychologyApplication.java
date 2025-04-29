1dcd0280bdebef42d3e2a2baf346422f4308ce2b
ViggaD2003 <thanhdanhvt2003@gmail.com>
2025-04-29 21:42:38 +0700

    Init Project BackEnd

---
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/SchoolPsychologyApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.gsu25se47.schoolpsychology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPsychologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolPsychologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/gsu25se47/schoolpsychology/SchoolPsychologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.gsu25se47.schoolpsychology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPsychologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
