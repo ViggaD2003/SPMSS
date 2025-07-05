ca4fdc5450c0d69572012745d1392b3b82faf56d
ViggaD2003 <thanhdanhvt2003@gmail.com>
2025-07-05 18:48:11 +0700

    update slot controller

--- src/main/java/com/fpt/gsu25se47/schoolpsychology/service/inter/AppointmentService.java
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/service/inter/AppointmentService.java
@@ -2,7 +2,6 @@ package com.fpt.gsu25se47.schoolpsychology.service.inter;
 
 import com.fpt.gsu25se47.schoolpsychology.dto.request.AddNewAppointment;
 import com.fpt.gsu25se47.schoolpsychology.dto.request.ConfirmAppointment;
-import com.fpt.gsu25se47.schoolpsychology.model.enums.AppointmentStatus;
 
 import java.util.Optional;
 
