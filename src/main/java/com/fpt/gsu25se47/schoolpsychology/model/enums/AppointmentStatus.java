4e0b65714f1058df16e4390d78c71ae2bdfc0196
nam127 <buithanhnam9109@gmail.com>
2025-07-10 12:15:19 +0700

    feat: add to cronjob for create appointment record based on appointment expired

--- src/main/java/com/fpt/gsu25se47/schoolpsychology/model/enums/AppointmentStatus.java
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/model/enums/AppointmentStatus.java
@@ -1,5 +1,5 @@
 package com.fpt.gsu25se47.schoolpsychology.model.enums;
 
 public enum AppointmentStatus {
-    PENDING, CONFIRMED, CANCELED, COMPLETED
+    PENDING, CONFIRMED, CANCELED, COMPLETED, EXPIRED
 }
