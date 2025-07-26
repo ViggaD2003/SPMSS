bc05f93d5d7c5cc214c482b68791e2d0169fd90d
nam127 <buithanhnam9109@gmail.com>
2025-07-27 02:59:11 +0700

    fix: mental evaluation mapper for lastEvaluatedAt field

--- src/main/java/com/fpt/gsu25se47/schoolpsychology/mapper/MentalEvaluationMapper.java
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/mapper/MentalEvaluationMapper.java
@@ -16,7 +16,7 @@ public interface MentalEvaluationMapper {
     MentalEvaluationResponse toMentalEvaluationResponse(MentalEvaluation mentalEvaluation);
 
     @Mapping(target = "source", constant = "APPOINTMENT")
-    @Mapping(target = "latestEvaluatedAt", expression = "java(appointment.getEndDateTime().toLocalDate())")
+    @Mapping(target = "latestEvaluatedAt", expression = "java(appointment.getUpdatedDate().toLocalDate())")
     @Mapping(target = "studentId",
             expression = "java(appointment.getBookedFor().getStudent() != null ? appointment.getBookedFor().getStudent().getId() : appointment.getBookedBy().getStudent().getId())")
     @Mapping(target = "appointmentId", source = "appointment.id")
