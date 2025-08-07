5b0dbe140aee3157f9a7de6edc5e6388662053c2
nam127 <buithanhnam9109@gmail.com>
2025-08-08 01:11:45 +0700

    fix: conflict -2

--- src/main/java/com/fpt/gsu25se47/schoolpsychology/repository/SupportProgramRepository.java
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/repository/SupportProgramRepository.java
@@ -1,12 +1,9 @@
 package com.fpt.gsu25se47.schoolpsychology.repository;
 
 import com.fpt.gsu25se47.schoolpsychology.model.SupportProgram;
-import com.fpt.gsu25se47.schoolpsychology.model.enums.ProgramStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface SupportProgramRepository extends JpaRepository<SupportProgram, Integer> {
