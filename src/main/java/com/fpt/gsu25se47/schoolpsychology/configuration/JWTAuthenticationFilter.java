51a783661621a0f6f5909c119f88103cc80e2d79
ViggaD2003 <thanhdanhvt2003@gmail.com>
2025-08-09 14:54:19 +0700

    fix jwt filter

--- src/main/java/com/fpt/gsu25se47/schoolpsychology/configuration/JWTAuthenticationFilter.java
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/configuration/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.fpt.gsu25se47.schoolpsychology.configuration;
 
 import com.fpt.gsu25se47.schoolpsychology.common.Status;
 import com.fpt.gsu25se47.schoolpsychology.model.Account;
+import com.fpt.gsu25se47.schoolpsychology.repository.TokenRepository;
 import com.fpt.gsu25se47.schoolpsychology.service.inter.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -24,6 +25,7 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     private final JWTService jwtService;
     private final UserDetailsService userDetailsService;
     private static final String AUTH_PATH_PREFIX = "/api/v1/auth";
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -49,20 +51,27 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             Account account = (Account) userDetailsService.loadUserByUsername(username);
 
+            var isTokenValid = tokenRepository.findByValue(accessToken)
+                    .map(token -> !token.getStatus().equalsIgnoreCase("expired"))
+                    .orElse(false);
+
+
             if (account == null || !account.getStatus()) {
                 filterChain.doFilter(request, response);
                 return;
             }
 
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                    account, null, account.getAuthorities()
-            );
+            if (!jwtService.checkIfExpired(accessToken) && isTokenValid) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        account, null, account.getAuthorities()
+                );
 
-            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authToken);
-        } else {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return;
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
         }
 
         filterChain.doFilter(request, response);
