e72304e54846cd70d304dbb39a21693278735894
nam127 <buithanhnam9109@gmail.com>
2025-08-12 12:33:57 +0700

    fix: adjust parent token students response

---
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/dto/response/StudentClaimDto.java
@@ -0,0 +1,32 @@
+package com.fpt.gsu25se47.schoolpsychology.dto.response;
+
+import com.fpt.gsu25se47.schoolpsychology.model.enums.Grade;
+import lombok.Data;
+
+@Data
+public class StudentClaimDto {
+
+    private Integer id;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String fullName;
+
+    private Boolean gender;
+
+    private String dob;
+
+    private String roleName;
+
+    private String studentCode;
+
+    private Boolean isEnableSurvey;
+
+    private Grade targetLevel;
+
+    private Integer teacherId;
+
+//    private ClassDto classDto;
+}
