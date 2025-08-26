1d878d159c5b720215ac6482cabbeec319d171ac
ViggaD2003 <thanhdanhvt2003@gmail.com>
2025-08-26 23:17:40 +0700

    feat chat room

--- src/main/java/com/fpt/gsu25se47/schoolpsychology/service/impl/ChatServiceImpl.java
+++ src/main/java/com/fpt/gsu25se47/schoolpsychology/service/impl/ChatServiceImpl.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService {
 
     private final ChatClient chatClient;
